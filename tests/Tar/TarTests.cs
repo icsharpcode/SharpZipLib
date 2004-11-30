6442819efd98ad5917b3e5d90fada36de6445f8b
John Reilly <john.f.reilly@gmail.com>
2004-11-30 12:14:39 +0000

    Added more tar tests

--- tests/Tar/TarTests.cs
+++ tests/Tar/TarTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 
 using NUnit.Framework;
 
@@ -9,7 +10,6 @@ namespace ICSharpCode.SharpZipLib.Tests.Tar {
 	
 	/// <summary>
 	/// This class contains test cases for Tar archive handling
-	/// TODO  A whole lot more tests
 	/// </summary>
 	[TestFixture]
 	public class TarTestSuite
@@ -44,6 +44,222 @@ namespace ICSharpCode.SharpZipLib.Tests.Tar {
 			tarIn.ProgressMessageEvent += new ProgressMessageHandler(EntryCounter);
 			tarIn.ListContents();
 			Assert.AreEqual(0, entryCount, "Expected 0 tar entries");
+		}
+		
+		/// <summary>
+		/// Test equals function for tar headers.
+		/// </summary>
+		[Test]
+		[Category("Tar")]
+		public void HeaderEquality()
+		{
+			TarHeader h1 = new TarHeader();
+			TarHeader h2 = new TarHeader();
+			
+			Assert.IsTrue(h1.Equals(h2));
+			
+			h1.Name = "ABCDEFG";
+			Assert.IsFalse(h1.Equals(h2));
+			h2.Name = h1.Name;
+			Assert.IsTrue(h1.Equals(h2));
+			
+			h1.Mode = 47;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.Mode = h1.Mode;
+			Assert.IsTrue(h1.Equals(h2));
+			
+			h1.UserId = 654;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.UserId = h1.UserId;
+			Assert.IsTrue(h1.Equals(h2));
+
+			h1.GroupId = 654;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.GroupId = h1.GroupId;
+			Assert.IsTrue(h1.Equals(h2));
+			
+			h1.Size = 654;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.Size = h1.Size;
+			Assert.IsTrue(h1.Equals(h2));
+		
+			h1.ModTime = DateTime.Now;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.ModTime = h1.ModTime;
+			Assert.IsTrue(h1.Equals(h2));
+			
+			h1.TypeFlag = 165;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.TypeFlag = h1.TypeFlag;
+			Assert.IsTrue(h1.Equals(h2));
+
+			h1.LinkName = "link";
+			Assert.IsFalse(h1.Equals(h2));
+			h2.LinkName = h1.LinkName;
+			Assert.IsTrue(h1.Equals(h2));
+		
+			h1.Magic = "ustar";
+			Assert.IsFalse(h1.Equals(h2));
+			h2.Magic = h1.Magic;
+			Assert.IsTrue(h1.Equals(h2));
+		
+			h1.Version = "1";
+			Assert.IsFalse(h1.Equals(h2));
+			h2.Version = h1.Version;
+			Assert.IsTrue(h1.Equals(h2));
+		
+			h1.UserName = "user";
+			Assert.IsFalse(h1.Equals(h2));
+			h2.UserName = h1.UserName;
+			Assert.IsTrue(h1.Equals(h2));
+		
+			h1.GroupName = "group";
+			Assert.IsFalse(h1.Equals(h2));
+			h2.GroupName = h1.GroupName;
+			Assert.IsTrue(h1.Equals(h2));
+		
+		
+			h1.DevMajor = 165;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.DevMajor = h1.DevMajor;
+			Assert.IsTrue(h1.Equals(h2));
+		
+			h1.DevMinor = 164;
+			Assert.IsFalse(h1.Equals(h2));
+			h2.DevMinor = h1.DevMinor;
+			Assert.IsTrue(h1.Equals(h2));
+			
+		}
+		
+		/// <summary>
+		/// Check that values set are preserved when writing and reading archives.
+		/// </summary>
+		[Test]
+		[Category("Tar")]
+		public void ValuesPreserved()
+		{
+			MemoryStream ms = new MemoryStream();
+			TarOutputStream tarOut = new TarOutputStream(ms);
+			
+			DateTime modTime = DateTime.Now;
+			
+			TarEntry entry = TarEntry.CreateTarEntry("TestEntry");
+			entry.GroupId = 12;
+			entry.UserId = 14;
+			entry.ModTime = modTime;
+			entry.UserName = "UserName";
+			entry.GroupName = "GroupName";
+			entry.TarHeader.Mode = 12345;
+			
+			tarOut.PutNextEntry(entry);
+			tarOut.Close();
+
+			MemoryStream ms2 = new MemoryStream();
+			ms2.Write(ms.GetBuffer(), 0, ms.GetBuffer().Length);
+			ms2.Seek(0, SeekOrigin.Begin);
+			
+			TarInputStream tarIn = new TarInputStream(ms2);
+			TarEntry nextEntry = tarIn.GetNextEntry();
+			Assert.AreEqual(entry.TarHeader.Checksum, nextEntry.TarHeader.Checksum, "Checksum");
+			
+			Assert.IsTrue(nextEntry.Equals(entry), "Entries should be equal");
+			Assert.IsTrue(nextEntry.TarHeader.Equals(entry.TarHeader), "Headers should match");
+			
+			// Tar only stores seconds 
+			DateTime truncatedTime = new DateTime(modTime.Year, modTime.Month, modTime.Day, 
+			                                      modTime.Hour, modTime.Minute, modTime.Second);
+			Assert.AreEqual(truncatedTime, nextEntry.ModTime, "Modtimes should match");
+			
+			int entryCount = 0;
+			while ( nextEntry != null )
+			{
+				++entryCount;
+				nextEntry = tarIn.GetNextEntry();
+			}
+			
+			Assert.AreEqual(1, entryCount, "Expected 1 entry");
+		}
+		
+		/// <summary>
+		/// Check invalid mod times are detected
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void InvalidModTime()
+		{
+			TarEntry e = TarEntry.CreateTarEntry("test");
+			e.ModTime = DateTime.MinValue;
+		}
+		
+		
+		/// <summary>
+		/// Check invalid sizes are detected
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void InvalidSize()
+		{
+			TarEntry e = TarEntry.CreateTarEntry("test");
+			e.Size = -6;
+		}
+		
+		/// <summary>
+		/// Check invalid names are detected
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void InvalidName()
+		{
+			TarEntry e = TarEntry.CreateTarEntry("test");
+			e.Name = null;
+		}
+		
+		/// <summary>
+		/// Check setting user and group names.
+		/// </summary>
+		[Test]
+		public void UserAndGroupNames()
+		{
+			TarEntry e = TarEntry.CreateTarEntry("test");
+			e.UserName = null;
+			Assert.IsNotNull(e.UserName, "Name set to OS default");
+			e.UserName = "";
+			Assert.AreEqual(0, e.UserName.Length, "Empty name allowed");
+			e.GroupName = null;
+			Assert.AreEqual("None", e.GroupName, "default group name is None");
+		}
+		
+		/// <summary>
+		/// Check invalid magic values are detected
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void InvalidMagic()
+		{
+			TarEntry e = TarEntry.CreateTarEntry("test");
+			e.TarHeader.Magic = null;
+		}
+		
+		/// <summary>
+		/// Check invalid link names are detected
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void InvalidLinkName()
+		{
+			TarEntry e = TarEntry.CreateTarEntry("test");
+			e.TarHeader.LinkName = null;
+		}
+		
+		/// <summary>
+		/// Check invalid version names are detected
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void InvalidVersionName()
+		{
+			TarEntry e = TarEntry.CreateTarEntry("test");
+			e.TarHeader.Version = null;
 		}
 	}
 }
