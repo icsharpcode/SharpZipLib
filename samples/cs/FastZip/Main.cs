8618115b20c29ca2df4a541ae5a8ce90ed7dc295
John Reilly <john.f.reilly@gmail.com>
2006-01-29 13:13:54 +0000

    Extend FastZip sample options

--- samples/cs/FastZip/Main.cs
+++ samples/cs/FastZip/Main.cs
@@ -80,6 +80,13 @@ namespace Samples.FastZipDemo
 				Console.WriteLine(e.Name);
 			}
 		}
+
+		bool ConfirmOverwrite(string file)
+		{
+			Console.WriteLine("Overwrite file {0} Y/N", file);
+			string yesNo = Console.ReadLine();
+			return yesNo.Trim().ToLower() == "y";
+		}
 		
 		void Run(string[] args)
 		{
@@ -91,6 +98,8 @@ namespace Samples.FastZipDemo
 			bool verbose = false;
 
 			bool createEmptyDirs = false;
+			FastZip.Overwrite overwrite = FastZip.Overwrite.Always;
+			FastZip.ConfirmOverwriteDelegate confirmOverwrite = null;
 			
 			Operation op = Operation.Unknown;
 			int argCount = 0;
@@ -168,6 +177,32 @@ namespace Samples.FastZipDemo
 							dirFilter = optArg;
 							break;
 							
+						case "o":
+						case "overwrite":
+							switch ( optArg )
+							{
+								case "always":
+									overwrite = FastZip.Overwrite.Always;
+									confirmOverwrite = null;
+									break;
+									
+								case "never":
+									overwrite = FastZip.Overwrite.Never;
+									confirmOverwrite = null;
+									break;
+									
+								case "prompt":
+									overwrite = FastZip.Overwrite.Prompt;
+									confirmOverwrite = new FastZip.ConfirmOverwriteDelegate(ConfirmOverwrite);
+									break;
+									
+								default:
+									Console.WriteLine("Invalid overwrite option");
+									op = Operation.Error;
+									break;
+							}
+							break;
+							
 						default:
 							Console.WriteLine("Unknown option {0}", args[i]);
 							op = Operation.Error;
@@ -194,12 +229,13 @@ namespace Samples.FastZipDemo
 			
 			FastZip sz = new FastZip(events);
 			sz.CreateEmptyDirectories = createEmptyDirs;
+			
 			switch ( op ) {
 				case Operation.Create:
 					if ( argCount == 2 ) {
 						Console.WriteLine("Creating Zip");
 
-						sz.CreateZip(arg1, arg2, recurse, fileFilter);
+						sz.CreateZip(arg1, arg2, recurse, fileFilter, dirFilter);
 					}
 					else
 						Console.WriteLine("Invalid arguments");
@@ -208,10 +244,10 @@ namespace Samples.FastZipDemo
 				case Operation.Extract:
 					if ( argCount == 2 ) {
 						Console.WriteLine("Extracting Zip");
-						sz.ExtractZip(arg1, arg2, fileFilter);
+						sz.ExtractZip(arg1, arg2, overwrite, confirmOverwrite, fileFilter, dirFilter, recurse);
 					}
 					else
-						Console.WriteLine("Invalid arguments");
+						Console.WriteLine("zipfile and target directory not specified");
 					break;
 					
 				case Operation.List:
@@ -228,7 +264,7 @@ namespace Samples.FastZipDemo
 					
 				case Operation.Unknown:
 					Console.WriteLine(
-					   "FastZip v0.1\n"
+					   "FastZip v0.2\n"
 					+  "  Usage: FastZip {options} operation args\n"
 					+  "Operation Options: (only one permitted)\n"
 					+  "  -x zipfile targetdir : Extract files from Zip\n"
@@ -241,6 +277,7 @@ namespace Samples.FastZipDemo
 					+  "  -e Process empty directories\n"
 					+  "  -r Recurse directories\n"
 					+  "  -v Verbose output\n"
+					+  "  -overwrite=prompt|always|never   : Overwrite on extract handling\n"
 					);
 					break;
 				
