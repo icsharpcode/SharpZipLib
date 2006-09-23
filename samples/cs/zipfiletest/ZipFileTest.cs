a66ad7969a3f3903c50443ee191ef204bc506b1f
John Reilly <john.f.reilly@gmail.com>
2006-09-23 05:44:00 +0000

    Rework ZipFileTest sample

--- samples/cs/zipfiletest/ZipFileTest.cs
+++ samples/cs/zipfiletest/ZipFileTest.cs
@@ -9,16 +9,27 @@ class MainClass
 {
 	static public void Main(string[] args)
 	{
-		ZipFile zFile = new ZipFile(args[0]);
-		Console.WriteLine("Listing of : " + zFile.Name);
-		Console.WriteLine("");
-		Console.WriteLine("Raw Size    Size      Date     Time     Name");
-		Console.WriteLine("--------  --------  --------  ------  ---------");
-		foreach (ZipEntry e in zFile) {
-			DateTime d = e.DateTime;
-			Console.WriteLine("{0, -10}{1, -10}{2}  {3}   {4}", e.Size, e.CompressedSize,
-			                                                    d.ToString("dd-MM-yy"), d.ToString("t"),
-			                                                    e.Name);
+		if ( args.Length < 1 ) {
+			Console.WriteLine("Usage: ZipList file");
+			return;
+		}
+		
+		if ( !File.Exists(args[0]) ) {
+			Console.WriteLine("Cannot find file");
+			return;
+		}
+		
+		using (ZipFile zFile = new ZipFile(args[0])) {
+			Console.WriteLine("Listing of : " + zFile.Name);
+			Console.WriteLine("");
+			Console.WriteLine("Raw Size    Size      Date     Time     Name");
+			Console.WriteLine("--------  --------  --------  ------  ---------");
+			foreach (ZipEntry e in zFile) {
+				DateTime d = e.DateTime;
+				Console.WriteLine("{0, -10}{1, -10}{2}  {3}   {4}", e.Size, e.CompressedSize,
+				                                                    d.ToString("dd-MM-yy"), d.ToString("HH:mm"),
+				                                                    e.Name);
+			}
 		}
 	}
 }
