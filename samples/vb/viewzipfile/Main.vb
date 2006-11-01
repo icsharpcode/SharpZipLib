db62a19c4d5e8ac20de1f2c01f588053f8db5b08
John Reilly <john.f.reilly@gmail.com>
2006-11-01 10:42:53 +0000

    Some corrections to VB sample ViewZipFile.

--- samples/vb/viewzipfile/Main.vb
+++ samples/vb/viewzipfile/Main.vb
@@ -2,22 +2,22 @@ Imports System
 Imports System.IO
 Imports System.Text
 Imports System.Collections
+Imports System.Windows.Forms
 Imports Microsoft.VisualBasic
 Imports ICSharpCode.SharpZipLib.Zip
-Imports ICSharpCode.SharpZipLib.GZip
 
-Public Class Form1
+Public Class MainForm
 	Inherits System.Windows.Forms.Form
 	Friend txtFileName As System.Windows.Forms.TextBox
 	Friend chkShowEntry As System.Windows.Forms.CheckBox
-	Friend txtContent As System.Windows.Forms.TextBox
-	Friend Label1 As System.Windows.Forms.Label
 	Friend btnView As System.Windows.Forms.Button
+	Friend Label1 As System.Windows.Forms.Label
+	Friend txtContent As System.Windows.Forms.TextBox
 	
 
 	Public Shared Sub Main()
-		Dim fForm1 As New Form1
-		fForm1.ShowDialog
+		Dim fMainForm As New MainForm
+		fMainForm.ShowDialog
 	End Sub
 	
 	Public Sub New()
@@ -31,27 +31,12 @@ Public Class Form1
 	End Sub
 
 	Private Sub InitializeComponent()
-			Me.btnView = New System.Windows.Forms.Button
-			Me.Label1 = New System.Windows.Forms.Label
 			Me.txtContent = New System.Windows.Forms.TextBox
+			Me.Label1 = New System.Windows.Forms.Label
+			Me.btnView = New System.Windows.Forms.Button
 			Me.chkShowEntry = New System.Windows.Forms.CheckBox
 			Me.txtFileName = New System.Windows.Forms.TextBox
 			Me.SuspendLayout
-			'
-			'btnView
-			'
-			Me.btnView.Location = New System.Drawing.Point(360, 16)
-			Me.btnView.Name = "btnView"
-			Me.btnView.TabIndex = 2
-			Me.btnView.Text = "View"
-			AddHandler Me.btnView.Click, AddressOf Me.BtnViewClick
-			'
-			'Label1
-			'
-			Me.Label1.Location = New System.Drawing.Point(24, 16)
-			Me.Label1.Name = "Label1"
-			Me.Label1.TabIndex = 4
-			Me.Label1.Text = "Zip File Name:"
 			'
 			'txtContent
 			'
@@ -62,37 +47,61 @@ Public Class Form1
 			Me.txtContent.TabIndex = 5
 			Me.txtContent.Text = ""
 			'
+			'Label1
+			'
+			Me.Label1.Location = New System.Drawing.Point(24, 16)
+			Me.Label1.Name = "Label1"
+			Me.Label1.TabIndex = 4
+			Me.Label1.Text = "Zip File Name:"
+			'
+			'btnView
+			'
+			Me.btnView.Location = New System.Drawing.Point(360, 16)
+			Me.btnView.Name = "btnView"
+			Me.btnView.TabIndex = 2
+			Me.btnView.Text = "View"
+			AddHandler Me.btnView.Click, AddressOf Me.BtnViewClick
+			'
 			'chkShowEntry
 			'
 			Me.chkShowEntry.Location = New System.Drawing.Point(24, 56)
 			Me.chkShowEntry.Name = "chkShowEntry"
 			Me.chkShowEntry.TabIndex = 1
-			Me.chkShowEntry.Text = "Show Entry"
+			Me.chkShowEntry.Text = "Show File Head"
 			'
 			'txtFileName
 			'
 			Me.txtFileName.Location = New System.Drawing.Point(144, 16)
 			Me.txtFileName.Name = "txtFileName"
-			Me.txtFileName.Size = New System.Drawing.Size(200, 22)
+			Me.txtFileName.Size = New System.Drawing.Size(200, 20)
 			Me.txtFileName.TabIndex = 0
 			Me.txtFileName.Text = ""
 			'
-			'Form1
+			'MainForm
 			'
-			Me.AutoScaleBaseSize = New System.Drawing.Size(6, 15)
+			Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
 			Me.ClientSize = New System.Drawing.Size(448, 296)
 			Me.Controls.Add(Me.txtContent)
 			Me.Controls.Add(Me.chkShowEntry)
 			Me.Controls.Add(Me.Label1)
 			Me.Controls.Add(Me.txtFileName)
 			Me.Controls.Add(Me.btnView)
-			Me.Name = "Form1"
+			Me.Name = "MainForm"
 			Me.Text = "View Zip file"
 			Me.ResumeLayout(false)
 		End Sub
 
-Private Sub BtnViewClick(sender As System.Object, e As System.EventArgs)
+	Private Sub BtnViewClick(sender As System.Object, e As System.EventArgs)
 
+		If txtFileName.Text.Trim().Length = 0
+			MessageBox.Show("Please enter a file name", "No file entered")
+			Return
+		End If
+		If Not File.Exists(txtFileName.Text)
+			MessageBox.Show(txtFileName.Text, "Cannot open file")
+			Return
+		End If
+		
 		Dim strmZipInputStream As ZipInputStream = New ZipInputStream(File.OpenRead(txtFileName.Text))
 		Dim objEntry As ZipEntry
 		Dim strOutput As String
@@ -110,16 +119,14 @@ Private Sub BtnViewClick(sender As System.Object, e As System.EventArgs)
 			Dim nSize As Integer = 2048
 			Dim abyData(2048) As Byte
 
-			If (True = chkShowEntry.Checked) Then
-				While True
-					nSize = strmZipInputStream.Read(abyData, 0, abyData.Length)
+			If (True = chkShowEntry.Checked) and objEntry.IsFile Then
+				nSize = strmZipInputStream.Read(abyData, 0, abyData.Length)
 
-					If nSize > 0 Then
-						'    strBuilder.Append(New ASCIIEncoding().GetString(abyData, 0, nSize) + vbCrLf)
-					Else
-						Exit While
-					End If
-				End While
+				If nSize > 0 Then
+					strBuilder.Append(New ASCIIEncoding().GetString(abyData, 0, nSize) + vbCrLf)
+					strBuilder.Append("---END---" + vbCrLf + vbCrLf)
+				End If
+				
 			End If
 
 			objEntry = strmZipInputStream.GetNextEntry()
@@ -127,7 +134,7 @@ Private Sub BtnViewClick(sender As System.Object, e As System.EventArgs)
 
 		txtContent.Text = strBuilder.ToString
 		strmZipInputStream.Close()
-End Sub
+	End Sub
 
 End Class
 
