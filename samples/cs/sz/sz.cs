e6c2a61ad0e5deeacd2e51211a1e7ad5df3f38f8
John Reilly <john.f.reilly@gmail.com>
2007-06-29 09:20:12 +0000

    Add new copyright and remove some old debug code.

--- samples/cs/sz/sz.cs
+++ samples/cs/sz/sz.cs
@@ -1,17 +1,32 @@
-//------------------------------------------------------------------------------
+// SharpZipLibrary samples
+// Copyright (c) 2007, AlphaSierraPapa
+// All rights reserved.
 //
-// sz - A command line archiver using SharpZipLib for actual compression
-//      Currently only handles Zip archives.
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
 //
-// Copyright 2004, 2005 John Reilly
+// - Redistributions of source code must retain the above copyright notice, this list
+//   of conditions and the following disclaimer.
 //
-//------------------------------------------------------------------------------
+// - Redistributions in binary form must reproduce the above copyright notice, this list
+//   of conditions and the following disclaimer in the documentation and/or other materials
+//   provided with the distribution.
+//
+// - Neither the name of the SharpDevelop team nor the names of its contributors may be used to
+//   endorse or promote products derived from this software without specific prior written
+//   permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &AS IS& AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Define test to add more detailed Console.WriteLine style information
 // #define TEST
-
-// Define this to get deeper detail on option handling
-// #define OPTIONTEST
 
 using System;
 using System.IO;
@@ -307,9 +322,6 @@ namespace SharpZip
 													try {
 														int enc = int.Parse(optArg);
 														if (Encoding.GetEncoding(enc) != null) {
-#if OPTIONTEST
-															Console.WriteLine("Encoding set to {0}", enc);
-#endif				
 															ZipConstants.DefaultCodePage = enc;
 														} else {
 															result = false;
@@ -452,9 +464,6 @@ namespace SharpZip
 					}
 				}
 				else {
-#if OPTIONTEST
-					Console.WriteLine("file spec {0} = '{1}'", argIndex, args[argIndex]);
-#endif				
 					fileSpecs.Add(args[argIndex]);
 				}
 				++argIndex;
