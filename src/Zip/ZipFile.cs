ed41ec9e9af39746202400e1b9b02f744efc58dc
John Reilly <john.f.reilly@gmail.com>
2007-11-30 20:30:45 +0000

    PartialInputStream no longer inherits from InflaterInputStream.  It supports seeking so nested ZipFiles will work for stored ZipFiles.  Some exceptions thrown had incorrect arguments.

--- src/Zip/ZipFile.cs
+++ src/Zip/ZipFile.cs
@@ -1528,7 +1528,7 @@ namespace ICSharpCode.SharpZipLib.Zip
 			}
 
 			if (!ZipEntry.IsCompressionMethodSupported(compressionMethod)) {
-				throw new ArgumentOutOfRangeException("Compression method not supported");
+				throw new ArgumentOutOfRangeException("compressionMethod");
 			}
 
 			CheckUpdating();
@@ -1555,7 +1555,7 @@ namespace ICSharpCode.SharpZipLib.Zip
 			}
 
 			if ( !ZipEntry.IsCompressionMethodSupported(compressionMethod) ) {
-				throw new ArgumentOutOfRangeException("Compression method not supported");
+				throw new ArgumentOutOfRangeException("compressionMethod");
 			}
 
 			CheckUpdating();
@@ -1739,7 +1739,7 @@ namespace ICSharpCode.SharpZipLib.Zip
 		public bool Delete(string fileName)
 		{
 			if ( fileName == null ) {
-				throw new ArgumentNullException("entryName");
+				throw new ArgumentNullException("fileName");
 			}
 			
 			CheckUpdating();
@@ -3539,20 +3539,65 @@ namespace ICSharpCode.SharpZipLib.Zip
 				}
 			}
 
+			/// <summary>
+			/// Reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
+			/// </summary>
+			/// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between offset and (offset + count - 1) replaced by the bytes read from the current source.</param>
+			/// <param name="offset">The zero-based byte offset in buffer at which to begin storing the data read from the current stream.</param>
+			/// <param name="count">The maximum number of bytes to be read from the current stream.</param>
+			/// <returns>
+			/// The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.
+			/// </returns>
+			/// <exception cref="T:System.ArgumentException">The sum of offset and count is larger than the buffer length. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support reading. </exception>
+			/// <exception cref="T:System.ArgumentNullException">buffer is null. </exception>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.ArgumentOutOfRangeException">offset or count is negative. </exception>
 			public override int Read(byte[] buffer, int offset, int count)
 			{
 				return 0;
 			}
 
+			/// <summary>
+			/// Sets the position within the current stream.
+			/// </summary>
+			/// <param name="offset">A byte offset relative to the origin parameter.</param>
+			/// <param name="origin">A value of type <see cref="T:System.IO.SeekOrigin"></see> indicating the reference point used to obtain the new position.</param>
+			/// <returns>
+			/// The new position within the current stream.
+			/// </returns>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support seeking, such as if the stream is constructed from a pipe or console output. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
 			public override long Seek(long offset, SeekOrigin origin)
 			{
 				return 0;
 			}
 
+			/// <summary>
+			/// Sets the length of the current stream.
+			/// </summary>
+			/// <param name="value">The desired length of the current stream in bytes.</param>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support both writing and seeking, such as if the stream is constructed from a pipe or console output. </exception>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
 			public override void SetLength(long value)
 			{
 			}
 
+			/// <summary>
+			/// Writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
+			/// </summary>
+			/// <param name="buffer">An array of bytes. This method copies count bytes from buffer to the current stream.</param>
+			/// <param name="offset">The zero-based byte offset in buffer at which to begin copying bytes to the current stream.</param>
+			/// <param name="count">The number of bytes to be written to the current stream.</param>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support writing. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			/// <exception cref="T:System.ArgumentNullException">buffer is null. </exception>
+			/// <exception cref="T:System.ArgumentException">The sum of offset and count is greater than the buffer length. </exception>
+			/// <exception cref="T:System.ArgumentOutOfRangeException">offset or count is negative. </exception>
 			public override void Write(byte[] buffer, int offset, int count)
 			{
 				baseStream_.Write(buffer, offset, count);
@@ -3567,7 +3612,7 @@ namespace ICSharpCode.SharpZipLib.Zip
 		/// A <see cref="PartialInputStream"/> is an <see cref="InflaterInputStream"/>
 		/// whose data is only a part or subsection of a file.
 		/// </summary>
-		class PartialInputStream : InflaterInputStream
+		class PartialInputStream : Stream
 		{
 			#region Constructors
 			/// <summary>
@@ -3577,49 +3622,16 @@ namespace ICSharpCode.SharpZipLib.Zip
 			/// <param name="start">The start of the partial data.</param>
 			/// <param name="length">The length of the partial data.</param>
 			public PartialInputStream(Stream baseStream, long start, long length)
-				: base(baseStream)
 			{
+				start_ = start;
+				length_ = length;
+				
 				baseStream_ = baseStream;
-				filepos_ = start;
+				readPos_ = start;
 				end_ = start + length;
 			}
 			
 			#endregion
-
-			/// <summary>
-			/// Skip the specified number of input bytes.
-			/// </summary>
-			/// <param name="count">The maximum number of input bytes to skip.</param>
-			/// <returns>The actuial number of input bytes skipped.</returns>
-			public long SkipBytes(long count)
-			{
-				if (count < 0) {
-#if NETCF_1_0					
-					throw new ArgumentOutOfRangeException("count");
-#else
-					throw new ArgumentOutOfRangeException("count", "is less than zero");
-#endif
-				}
-				
-				if (count > end_ - filepos_) {
-					count = end_ - filepos_;
-				}
-				
-				filepos_ += count;
-				return count;
-			}
-
-			public override int Available 
-			{
-				get {
-					long amount = end_ - filepos_;
-					if (amount > int.MaxValue) {
-						return int.MaxValue;
-					}
-					
-					return (int) amount;
-				}
-			}
 
 			/// <summary>
 			/// Read a byte from this stream.
@@ -3627,13 +3639,13 @@ namespace ICSharpCode.SharpZipLib.Zip
 			/// <returns>Returns the byte read or -1 on end of stream.</returns>
 			public override int ReadByte()
 			{
-				if (filepos_ == end_) {
+				if (readPos_ >= end_) {
 					 // -1 is the correct value at end of stream.
 					return -1;
 				}
 				
 				lock( baseStream_ ) {
-					baseStream_.Seek(filepos_++, SeekOrigin.Begin);
+					baseStream_.Seek(readPos_++, SeekOrigin.Begin);
 					return baseStream_.ReadByte();
 				}
 			}
@@ -3648,29 +3660,196 @@ namespace ICSharpCode.SharpZipLib.Zip
 			{
 				// Do nothing at all!
 			}
-			
+
+			/// <summary>
+			/// Reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
+			/// </summary>
+			/// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between offset and (offset + count - 1) replaced by the bytes read from the current source.</param>
+			/// <param name="offset">The zero-based byte offset in buffer at which to begin storing the data read from the current stream.</param>
+			/// <param name="count">The maximum number of bytes to be read from the current stream.</param>
+			/// <returns>
+			/// The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.
+			/// </returns>
+			/// <exception cref="T:System.ArgumentException">The sum of offset and count is larger than the buffer length. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support reading. </exception>
+			/// <exception cref="T:System.ArgumentNullException">buffer is null. </exception>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.ArgumentOutOfRangeException">offset or count is negative. </exception>
 			public override int Read(byte[] buffer, int offset, int count)
 			{
-				if (count > end_ - filepos_) {
-					count = (int) (end_ - filepos_);
-					if (count == 0) {
-						return 0;
-					}
-				}
-				
 				lock(baseStream_) {
-					baseStream_.Seek(filepos_, SeekOrigin.Begin);
+					if (count > end_ - readPos_) {
+						count = (int) (end_ - readPos_);
+						if (count == 0) {
+							return 0;
+						}
+					}
+					
+					baseStream_.Seek(readPos_, SeekOrigin.Begin);
 					int readCount = baseStream_.Read(buffer, offset, count);
 					if (readCount > 0) {
-						filepos_ += readCount;
+						readPos_ += readCount;
 					}
 					return readCount;
 				}
 			}
+
+			/// <summary>
+			/// Writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
+			/// </summary>
+			/// <param name="buffer">An array of bytes. This method copies count bytes from buffer to the current stream.</param>
+			/// <param name="offset">The zero-based byte offset in buffer at which to begin copying bytes to the current stream.</param>
+			/// <param name="count">The number of bytes to be written to the current stream.</param>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support writing. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			/// <exception cref="T:System.ArgumentNullException">buffer is null. </exception>
+			/// <exception cref="T:System.ArgumentException">The sum of offset and count is greater than the buffer length. </exception>
+			/// <exception cref="T:System.ArgumentOutOfRangeException">offset or count is negative. </exception>
+			public override void Write(byte[] buffer, int offset, int count)
+			{
+				throw new NotSupportedException();
+			}
+
+			/// <summary>
+			/// When overridden in a derived class, sets the length of the current stream.
+			/// </summary>
+			/// <param name="value">The desired length of the current stream in bytes.</param>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support both writing and seeking, such as if the stream is constructed from a pipe or console output. </exception>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			public override void SetLength(long value)
+			{
+				throw new NotSupportedException();
+			}
+
+			/// <summary>
+			/// When overridden in a derived class, sets the position within the current stream.
+			/// </summary>
+			/// <param name="offset">A byte offset relative to the origin parameter.</param>
+			/// <param name="origin">A value of type <see cref="T:System.IO.SeekOrigin"></see> indicating the reference point used to obtain the new position.</param>
+			/// <returns>
+			/// The new position within the current stream.
+			/// </returns>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support seeking, such as if the stream is constructed from a pipe or console output. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			public override long Seek(long offset, SeekOrigin origin)
+			{
+				long newPos = readPos_;
+				
+				switch ( origin )
+				{
+					case SeekOrigin.Begin:
+						newPos = start_ + offset;
+						break;
+						
+					case SeekOrigin.Current:
+						newPos = readPos_ + offset;
+						break;
+						
+					case SeekOrigin.End:
+						newPos = end_ + offset;
+						break;
+				}
+				
+				if ( newPos < start_ ) {
+					throw new ArgumentException("Negative position is invalid");
+				}
+				
+				if ( newPos >= end_ ) {
+					throw new IOException("Cannot seek past end");
+				}
+				readPos_ = newPos;
+				return readPos_;
+			}
+
+			/// <summary>
+			/// Clears all buffers for this stream and causes any buffered data to be written to the underlying device.
+			/// </summary>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			public override void Flush()
+			{
+				// Nothing to do.
+			}
+
+			/// <summary>
+			/// Gets or sets the position within the current stream.
+			/// </summary>
+			/// <value></value>
+			/// <returns>The current position within the stream.</returns>
+			/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+			/// <exception cref="T:System.NotSupportedException">The stream does not support seeking. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			public override long Position {
+				get { return readPos_ - start_; }
+				set { 
+					long newPos = start_ + value;
+					
+					if ( newPos < start_ ) {
+						throw new ArgumentException("Negative position is invalid");
+					}
+					
+					if ( newPos >= end_ ) {
+						throw new InvalidOperationException("Cannot seek past end");
+					}
+					readPos_ = newPos;
+				}
+			}
+
+			/// <summary>
+			/// Gets the length in bytes of the stream.
+			/// </summary>
+			/// <value></value>
+			/// <returns>A long value representing the length of the stream in bytes.</returns>
+			/// <exception cref="T:System.NotSupportedException">A class derived from Stream does not support seeking. </exception>
+			/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+			public override long Length {
+				get { return length_; }
+			}
+
+			/// <summary>
+			/// Gets a value indicating whether the current stream supports writing.
+			/// </summary>
+			/// <value>false</value>
+			/// <returns>true if the stream supports writing; otherwise, false.</returns>
+			public override bool CanWrite {
+				get { return false; }
+			}
+
+			/// <summary>
+			/// Gets a value indicating whether the current stream supports seeking.
+			/// </summary>
+			/// <value>true</value>
+			/// <returns>true if the stream supports seeking; otherwise, false.</returns>
+			public override bool CanSeek {
+				get { return true; }
+			}
+
+			/// <summary>
+			/// Gets a value indicating whether the current stream supports reading.
+			/// </summary>
+			/// <value>true.</value>
+			/// <returns>true if the stream supports reading; otherwise, false.</returns>
+			public override bool CanRead {
+				get { return true; }
+			}
+
+			/// <summary>
+			/// Gets a value that determines whether the current stream can time out.
+			/// </summary>
+			/// <value></value>
+			/// <returns>A value that determines whether the current stream can time out.</returns>
+			public override bool CanTimeout {
+				get { return baseStream_.CanTimeout; }
+			}
 			
 			#region Instance Fields
 			Stream baseStream_;
-			long filepos_;
+			long start_;
+			long length_;
+			long readPos_;
 			long end_;
 			#endregion	
 		}
@@ -4006,16 +4185,16 @@ namespace ICSharpCode.SharpZipLib.Zip
 		/// <summary>
 		/// Return a stream suitable for performing direct updates on the original source.
 		/// </summary>
-		/// <param name="current">The current stream.</param>
+		/// <param name="stream">The current stream.</param>
 		/// <returns>Returns a stream suitable for direct updating.</returns>
 		/// <remarks>If the <paramref name="current"/> stream is not null this is used as is.</remarks>
-		public override Stream OpenForDirectUpdate(Stream current)
+		public override Stream OpenForDirectUpdate(Stream stream)
 		{
 			Stream result;
-			if ((current == null) || !current.CanWrite)
+			if ((stream == null) || !stream.CanWrite)
 			{
-				if (current != null) {
-					current.Close();
+				if (stream != null) {
+					stream.Close();
 				}
 
 				result = new FileStream(fileName_,
@@ -4024,7 +4203,7 @@ namespace ICSharpCode.SharpZipLib.Zip
 			}
 			else
 			{
-				result = current;
+				result = stream;
 			}
 
 			return result;
@@ -4043,7 +4222,7 @@ namespace ICSharpCode.SharpZipLib.Zip
 		#endregion
 
 		#region Internal routines
-		string GetTempFileName(string original, bool makeTempFile)
+		static string GetTempFileName(string original, bool makeTempFile)
 		{
 			string result = null;
 				
