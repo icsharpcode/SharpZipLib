6da2d63a63ca48af71e2a9136c0b0618a2380652
John Reilly <john.f.reilly@gmail.com>
2010-04-04 20:20:50 +0000

    Correct indenting

--- src/Zip/Compression/InflaterDynHeader.cs
+++ src/Zip/Compression/InflaterDynHeader.cs
@@ -178,8 +178,8 @@ namespace ICSharpCode.SharpZipLib.Zip.Compression
 								return true;
 							}
 						}
-							mode = LENS;
-							goto decode_loop;
+						mode = LENS;
+						goto decode_loop;
 					}
 				}
 		}
@@ -204,6 +204,9 @@ namespace ICSharpCode.SharpZipLib.Zip.Compression
 
 		InflaterHuffmanTree blTree;
 
+        /// <summary>
+        /// The current decode mode
+        /// </summary>
 		int mode;
 		int lnum, dnum, blnum, num;
 		int repSymbol;
