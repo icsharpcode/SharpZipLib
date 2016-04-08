42e72f5f467ae34112af15a542814402b809f4d1
Neil McNeight <mcneight+github@gmail.com>
2016-04-07 18:46:32 -0700

    More CI Enhancements * Added .travis.yml as second CI source * Expanded appveyor.yml with more build settings * Added travis badge next to appveyor badge * Updated settings in ICSharpCode.SharpZipLib.Tests.csproj and ICSharpCode.SharpZipLib.sln to support automated testing on travis

--- ICSharpCode.SharpZipLib/AssemblyInfo.cs
+++ ICSharpCode.SharpZipLib/AssemblyInfo.cs
@@ -20,7 +20,7 @@
 // making a combined work based on this library.  Thus, the terms and
 // conditions of the GNU General Public License cover the whole
 // combination.
-// 
+//
 // As a special exception, the copyright holders of this library give you
 // permission to link this library with independent modules to produce an
 // executable, regardless of the license terms of these independent
@@ -79,7 +79,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("0.87.*")]
-[assembly: AssemblyInformationalVersionAttribute("0.86.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.87.*")]
 
 
 [assembly: CLSCompliant(true)]
@@ -91,46 +91,7 @@ using System.Runtime.InteropServices;
 [assembly: System.Security.AllowPartiallyTrustedCallers]
 #endif
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
-
-//
-// In order to sign your assembly you must specify a key to use. Refer to the 
-// Microsoft .NET Framework documentation for more information on assembly signing.
-//
-// Use the attributes below to control which key is used for signing. 
-//
-// Notes: 
-//   (*) If no key is specified, the assembly is not signed.
-//   (*) KeyName refers to a key that has been installed in the Crypto Service
-//       Provider (CSP) on your machine. KeyFile refers to a file which contains
-//       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the 
-//       following processing occurs:
-//       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
-//           in the KeyFile is installed into the CSP and used.
-//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
-//       When specifying the KeyFile, the location of the KeyFile should be
-//       relative to the project output directory which is
-//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
-//       located in the project directory, you would specify the AssemblyKeyFile 
-//       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
-//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
-//       documentation for more information on this.
-//
-#if (CLI_1_0 || NET_1_0 || NET_1_1 || NETCF_1_0 || SSCLI)
-[assembly: AssemblyDelaySign(false)]
-#if VSTUDIO
-[assembly: AssemblyKeyFile("../../ICSharpCode.SharpZipLib.key")]
-#elif AUTOBUILD
-[assembly: AssemblyKeyFile("ICSharpCode.SharpZipLib.key")]
-#else
-[assembly: AssemblyKeyFile("../ICSharpCode.SharpZipLib.key")]
-#endif
-#endif
-
-
