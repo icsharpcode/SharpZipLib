53d60da2728210b99e336bf65c700341e0138634
Neil McNeight <mcneight+github@gmail.com>
2016-04-26 00:03:16 -0700

    Refactoring Samples to Usable Utilities * renamed minibzip2 to Cmd_BZip2 * renamed minigzip to Cmd_GZip * renamed zipfiletest to Cmd_ZipInfo

--- ICSharpCode.SharpZipLib.Samples/cs/minibzip2/AssemblyInfo.cs
+++ ICSharpCode.SharpZipLib.Samples/cs/minibzip2/AssemblyInfo.cs
@@ -1,16 +1,8 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("SharpDevelop")]
-[assembly: AssemblyDescription("free C# IDE")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpDevelop")]
-[assembly: AssemblyCopyright("Copyright © 2000-2016 AlphaSierraPapa for the SharpZipLib Team")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("Cmd_BZip2")]
+[assembly: AssemblyDescription("bzip2 based file compression")]
 [assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("0.85.4.369")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
