6250f4906f04bdf48b38149e00c93eb7b686292e
Neil McNeight <mcneight+github@gmail.com>
2016-05-17 17:09:18 -0700

    Complete Documentation Cycle * Execute the following commands in order:   * run-nunit3-tests-release.cmd to get test report   * run-opencover.cmd to get test coverage   * run-reportgenerator.cmd to get HTML report of test coverage * Modify appveyor.yml to reference .cmd files

--- Build/run-nunit3-tests-release.cmd
+++ Build/run-nunit3-tests-release.cmd
@@ -1,1 +1,1 @@
-..\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe /framework:net-4.5 /result:..\Documentation\nunit3-test-results-release.xml ..\bin\Release\ICSharpCode.SharpZipLib.Tests.dll
+packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe --framework=net-4.5 --domain=Single --testlist=ICSharpCode.SharpZipLib.Tests\PassingTests.txt --result=Documentation\nunit3-test-results-release.xml bin\Release\ICSharpCode.SharpZipLib.Tests.dll
---
+++ Build/run-reportgenerator.cmd
@@ -0,0 +1,1 @@
+packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe -reports:Documentation\opencover-results-release.xml -targetdir:Documentation\opencover
