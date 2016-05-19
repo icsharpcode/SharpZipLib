a63e43a21407003dbc04ad7adce15f51aa1c9b65
Neil McNeight <mcneight+github@gmail.com>
2016-05-19 00:01:52 -0700

    BadgesBadgesBadgesBadges * Added a Coverity badge * Added a condition to only send scheduled builds to Coverity * Install PublishCoverity from NuGet * Remove tag push for GitHub * Add coverity.zip as build artifact

--- Build/run-appveyor-build.ps1
+++ Build/run-appveyor-build.ps1
@@ -1,4 +1,3 @@
-$publishCoverityExe = $env:APPVEYOR_BUILD_FOLDER + "\packages\PublishCoverity.0.11.0\tools\PublishCoverity.exe";
 # Define build command.
 $buildCmd = "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe";
 $buildArgs = @(
@@ -10,13 +9,14 @@ $buildArgs = @(
               "/p:Platform=Any CPU");
 
 # If build is not a scheduled one, then simply build the project with MSBuild.
-#if ($env:APPVEYOR_SCHEDULED_BUILD -ne "True") {
-#  & $buildCmd $buildArgs
+if ($env:APPVEYOR_SCHEDULED_BUILD -ne "True") {
+  & $buildCmd $buildArgs
 #  & nuget pack <project_file> -OutputDirectory <temp_path>
-#  return  # exit script
-#}
+  return  # exit script
+}
 
 # Else, build project with Coverity Scan.
+$publishCoverityExe = $env:APPVEYOR_BUILD_FOLDER + "\packages\PublishCoverity.0.11.0\tools\PublishCoverity.exe";
 "Building project with Coverity Scan..."
 & cov-build --dir Documentation\cov-int $buildCmd $buildArgs;
 
@@ -25,6 +25,3 @@ $buildArgs = @(
 
 # Upload scan data.
 & $publishCoverityExe publish -z Documentation\coverity.zip -r McNeight/SharpZipLib -t $env:Coverity_Token -e $env:Coverity_Email -d "AppVeyor scheduled build";
-
-
-
