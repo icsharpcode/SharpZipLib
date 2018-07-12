66f0a2d964f0300d3a3c1346ff674e7b1565d6ce
nils måsén <nils@piksel.se>
2018-07-12 10:16:51 +0200

    Add test for #213
    Add disposable IO utils

--- test/ICSharpCode.SharpZipLib.Tests/TestSupport/Utils.cs
+++ test/ICSharpCode.SharpZipLib.Tests/TestSupport/Utils.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace ICSharpCode.SharpZipLib.Tests.TestSupport
@@ -8,6 +9,7 @@ namespace ICSharpCode.SharpZipLib.Tests.TestSupport
 	/// </summary>
 	public static class Utils
 	{
+		static Random random = new Random();
 
 		static void Compare(byte[] a, byte[] b)
 		{
@@ -25,5 +27,97 @@ namespace ICSharpCode.SharpZipLib.Tests.TestSupport
 			}
 		}
 
+		public static TempFile GetDummyFile(int size = -1)
+		{
+			var tempFile = new TempFile();
+			if (size < 0)
+			{
+				File.WriteAllText(tempFile.Filename, DateTime.UtcNow.Ticks.ToString("x16"));
+			}
+			else if (size > 0)
+			{
+				var bytes = Array.CreateInstance(typeof(byte), size) as byte[];
+				random.NextBytes(bytes);
+				File.WriteAllBytes(tempFile.Filename, bytes);
+			}
+			return tempFile;
+		}
+
+		public class TempFile : IDisposable
+		{
+			public string Filename { get; internal set; }
+
+			public TempFile()
+			{
+				Filename = Path.GetTempFileName();
+			}
+
+			#region IDisposable Support
+			private bool disposed = false; // To detect redundant calls
+
+
+			protected virtual void Dispose(bool disposing)
+			{
+				if (!disposed)
+				{
+					if (disposing && File.Exists(Filename))
+					{
+						try
+						{
+							File.Delete(Filename);
+						}
+						catch { }
+					}
+
+					disposed = true;
+				}
+			}
+
+			public void Dispose()
+				=> Dispose(true);
+
+			#endregion
+
+		}
+
+		public class TempDir : IDisposable
+		{
+			public string Fullpath { get; internal set; }
+
+			public TempDir()
+			{
+				Fullpath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+				Directory.CreateDirectory(Fullpath);
+			}
+
+			#region IDisposable Support
+			private bool disposed = false; // To detect redundant calls
+
+
+			protected virtual void Dispose(bool disposing)
+			{
+				if (!disposed)
+				{
+					if (disposing && Directory.Exists(Fullpath))
+					{
+						try
+						{
+							Directory.Delete(Fullpath, true);
+						}
+						catch { }
+					}
+
+					disposed = true;
+				}
+			}
+
+			public void Dispose()
+				=> Dispose(true);
+
+			#endregion
+
+		}
 	}
+
+	
 }
