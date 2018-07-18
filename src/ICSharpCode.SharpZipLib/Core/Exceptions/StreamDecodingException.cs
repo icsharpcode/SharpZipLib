28e14cc49e790534a306f962ed63cc0f09886365
nils måsén <nils@piksel.se>
2018-07-19 00:10:21 +0200

    Merge PR #248, Add generic decoding exception classes

---
+++ src/ICSharpCode.SharpZipLib/Core/Exceptions/StreamDecodingException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICSharpCode.SharpZipLib
+{
+    /// <summary>
+    /// Indicates that an error occured during decoding of a input stream due to corrupt 
+	/// data or (unintentional) library incompability.
+    /// </summary>
+    public class StreamDecodingException: SharpZipBaseException
+    {
+		private const string GenericMessage = "Input stream could not be decoded";
+
+		/// <summary>
+		/// Initializes a new instance of the StreamDecodingException with a generic message
+		/// </summary>
+		public StreamDecodingException() : base(GenericMessage) { }
+
+		/// <summary>
+		/// Initializes a new instance of the StreamDecodingException class with a specified error message.
+		/// </summary>
+		/// <param name="message">A message describing the exception.</param>
+		public StreamDecodingException(string message) : base(message) { }
+
+
+		/// <summary>
+		/// Initializes a new instance of the StreamDecodingException class with a specified
+		/// error message and a reference to the inner exception that is the cause of this exception.
+		/// </summary>
+		/// <param name="message">A message describing the exception.</param>
+		/// <param name="innerException">The inner exception</param>
+		public StreamDecodingException(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
---
+++ src/ICSharpCode.SharpZipLib/Core/Exceptions/ValueOutOfRangeException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICSharpCode.SharpZipLib
+{
+
+	/// <summary>
+	/// Indicates that a value was outside of the expected range when decoding an input stream
+	/// </summary>
+	public class ValueOutOfRangeException : StreamDecodingException
+	{
+		/// <summary>
+		/// Initializes a new instance of the ValueOutOfRangeException class naming the the causing variable
+		/// </summary>
+		/// <param name="nameOfValue">Name of the variable, use: nameof()</param>
+		public ValueOutOfRangeException(string nameOfValue ) 
+			: base($"{nameOfValue} out of range") { }
+
+		/// <summary>
+		/// Initializes a new instance of the ValueOutOfRangeException class naming the the causing variable,
+		/// it's current value and expected range.
+		/// </summary>
+		/// <param name="nameOfValue">Name of the variable, use: nameof()</param>
+		/// <param name="value">The invalid value</param>
+		/// <param name="maxValue">Expected maximum value</param>
+		/// <param name="minValue">Expected minimum value</param>
+		public ValueOutOfRangeException(string nameOfValue, long value, long maxValue, long minValue = 0) 
+			: this(nameOfValue, value.ToString(), maxValue.ToString(), minValue.ToString()) { }
+
+		/// <summary>
+		/// Initializes a new instance of the ValueOutOfRangeException class naming the the causing variable,
+		/// it's current value and expected range.
+		/// </summary>
+		/// <param name="nameOfValue">Name of the variable, use: nameof()</param>
+		/// <param name="value">The invalid value</param>
+		/// <param name="maxValue">Expected maximum value</param>
+		/// <param name="minValue">Expected minimum value</param>
+		public ValueOutOfRangeException(string nameOfValue, string value, string maxValue, string minValue = "0") :
+			base($"{nameOfValue} out of range: {value}, should be {minValue}..{maxValue}") { }
+
+		private ValueOutOfRangeException() { }
+		private ValueOutOfRangeException(string message, Exception innerException) : base(message, innerException) {}
+	}
+}
