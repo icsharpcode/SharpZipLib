07444395b411d876c52c5c93a4130e5bfac44526
nils måsén <nils@piksel.se>
2018-09-16 15:26:23 +0200

    Revert AES constructor to generic

--- src/ICSharpCode.SharpZipLib/Encryption/ZipAESTransform.cs
+++ src/ICSharpCode.SharpZipLib/Encryption/ZipAESTransform.cs
@@ -78,7 +78,7 @@ namespace ICSharpCode.SharpZipLib.Encryption
 
 			// Performs the equivalent of derive_key in Dr Brian Gladman's pwd2key.c
 			var pdb = new Rfc2898DeriveBytes(key, saltBytes, KEY_ROUNDS);
-			var rm = new AesManaged();
+			var rm = Aes.Create();
 			rm.Mode = CipherMode.ECB;           // No feedback from cipher for CTR mode
 			_counterNonce = new byte[_blockSize];
 			byte[] key1bytes = pdb.GetBytes(_blockSize);
