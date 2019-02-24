cd5a9f0956b92f0bf37b9fd66d0f5ba2c305c1fd
Richard Webb <webby@beardmouse.co.uk>
2019-02-24 16:49:57 +0000

    Add a benchmarks project using BenchmarkDotNet

---
+++ benchmark/ICSharpCode.SharpZipLib.Benchmark/Checksum/BZip2Crc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace ICSharpCode.SharpZipLib.Benchmark.Checksum
+{
+	[Config(typeof(MultipleRuntimes))]
+	public class BZip2Crc
+	{
+		private const int ChunkCount = 256;
+		private const int ChunkSize = 1024 * 1024;
+		private const int N = ChunkCount * ChunkSize;
+		private readonly byte[] data;
+
+		public BZip2Crc()
+		{
+			data = new byte[N];
+			new Random(1).NextBytes(data);
+		}
+
+		[Benchmark]
+		public long BZip2CrcLargeUpdate()
+		{
+			var bzipCrc = new ICSharpCode.SharpZipLib.Checksum.BZip2Crc();
+			bzipCrc.Update(data);
+			return bzipCrc.Value;
+		}
+
+		/*
+		[Benchmark]
+		public long BZip2CrcChunkedUpdate()
+		{
+			var bzipCrc = new ICSharpCode.SharpZipLib.Checksum.BZip2Crc();
+
+			for (int i = 0; i < ChunkCount; i++)
+			{
+				var segment = new ArraySegment<byte>(data, ChunkSize * i, ChunkSize);
+				bzipCrc.Update(segment);
+			}
+
+			return bzipCrc.Value;
+		}
+		*/
+	}
+}
